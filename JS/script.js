31a3c993ca67717755054fc6c84713cb3121e247
RobertSajdak <robert.sajdak@onet.eu>
2022-05-25 21:32:31 +0200

    adding new tasks and showing if a task is done

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,19 @@
 {
     const tasks = [
         {
-            content: "przerobić lekcję",
+            content: "przerobić lekcję JS",
             done: false,
         },
         {
             content: "spacer z psem",
+            done: true,
+        },
+        {
+            content: "zjeść obiad",
+            done: true,
+        },
+        {
+            content: "spotkanie w biurze",
             done: true,
         },
     ];
@@ -15,18 +23,42 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li>
+                <li
+                    ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+                >
                     ${task.content}
                 </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);     
     };
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
